b9c4d5976c0af9a37276bcf6c097acddc11f43eb
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-29 22:47:21 +0300

    creat list in interface MessageRepo

--- src/main/java/com/example/courseworkrsoi/repos/MessageRepo.java
+++ src/main/java/com/example/courseworkrsoi/repos/MessageRepo.java
@@ -1,5 +1,10 @@
 package com.example.courseworkrsoi.repos;
 
-public interface MessageRepo {
+import com.example.courseworkrsoi.domain.Message;
+import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message,Long> {
+    List<Message> findByTag(String tag);
 }
