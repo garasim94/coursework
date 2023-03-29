5348185d6fe95634fca8adbb90e9cf683b9fc3fc
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-29 22:31:29 +0300

    first commit

---
+++ src/main/java/com/example/courseworkrsoi/CourseworkRsoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseworkrsoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkRsoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkRsoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/courseworkrsoi/CourseworkRsoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworkrsoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkRsoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
