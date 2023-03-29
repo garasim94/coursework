c400e5750cc9915f42113d6c1303ba1308853242
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-30 00:32:12 +0300

    update class MvcConfig

--- src/main/java/com/example/courseworkrsoi/config/MvcConfig.java
+++ src/main/java/com/example/courseworkrsoi/config/MvcConfig.java
@@ -1,4 +1,10 @@
 package com.example.courseworkrsoi.config;
 
-public class MvcConfig {
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
 }
