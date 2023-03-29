fcb61c597d165026687ec1969454a2cccdb7c1a5
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-30 00:14:34 +0300

    update GreetingController

--- src/main/java/com/example/courseworkrsoi/GreetingController.java
+++ src/main/java/com/example/courseworkrsoi/GreetingController.java
@@ -28,19 +28,27 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
+    @PostMapping("/main")
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message= new Message(text,tag);
+        messageRepo.save(message);
+        //Iterable<Message> messages;
+        Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
 
         return "main";
     }
 
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+
+        return "main";
+    }
 }
