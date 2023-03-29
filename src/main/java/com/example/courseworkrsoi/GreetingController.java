908812089b8ed9c8a509e9be4bd41e9ef1485026
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-29 22:55:16 +0300

    creat var messageRepo

--- src/main/java/com/example/courseworkrsoi/GreetingController.java
+++ src/main/java/com/example/courseworkrsoi/GreetingController.java
@@ -1,5 +1,8 @@
 package com.example.courseworkrsoi;
 
+import com.example.courseworkrsoi.domain.Message;
+import com.example.courseworkrsoi.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +11,9 @@ import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required = false,defaultValue ="World")
                            String name, Map<String,Object> model){
@@ -16,7 +22,8 @@ public class GreetingController {
     }
     @GetMapping("/main")
     public String main(Map<String,Object> model){
-        model.put("some","hello, letsCode!");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages",messages);
         return "main";
     }
 
